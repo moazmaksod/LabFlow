c096ef5dedc3868e1aa7c8d963492d9f514ffeb4
MoazDev <moaz.maksod@gmail.com>
2025-09-22 16:07:06 +0000

    still not work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
             <div className="flex">
               <AppSidebar />
               <div className="flex-1">
-                <SidebarInset>
+                <SidebarInset className="flex">
                   <Header />
                   <main className="flex-1 p-4 sm:p-6 lg:p-8">{children}</main>
                 </SidebarInset>
