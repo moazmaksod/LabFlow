bf6b9fa215ae70bf6728871d9b300339b8f2f040
MoazDev <moaz.maksod@gmail.com>
2025-09-22 16:28:57 +0000

    not fixed try to lock first for all bugs that may coasing this not loock

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -327,6 +327,7 @@ const SidebarInset = React.forwardRef<
       ref={ref}
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
+        "md:peer-data-[state=expanded]:peer-data-[variant=inset]:w-[calc(100%-16rem)]",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -765,5 +766,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
-    
